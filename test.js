809cffb6c61860b6f1432c21d99c15febe9ac1c2
ayushgupta108 <ayush.gupta@testlify.com>
2026-05-19 19:05:02 +0530

    Testing

---
+++ test.js
@@ -0,0 +1,14 @@
+// test
+// test
+const abc = 123;
+const xyz = 456;
+
+console.log(abc + xyzz);
+
+const def = 789;
+
+if (abc =- 10) {
+    console.log("wrong condition");
+}
+
+console.log(abc + xyz + def.toUpperCase());     
